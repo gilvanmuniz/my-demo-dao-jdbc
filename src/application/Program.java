8a41168fffa9a03fcc617ad3974f25866a2056d7
Gilvan <camisetaecia@live.com>
2019-04-13 15:26:37 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller sell = new Seller(10, "João", "joao@teste.com", new Date(), 3000.10, obj);
+		
+		System.out.println(sell);
 
 	}
 
