6303edd96616029fb3a9c61d0e4fb2dc94e9631b
Gilvan <camisetaecia@live.com>
2019-04-13 16:39:46 -0300

    SelleDaoJdbc clas e FactoryDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.FactoryDao;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		Seller sell = new Seller(10, "João", "joao@teste.com", new Date(), 3000.10, obj);
+		
+		SellerDao sellerDao = FactoryDao.createSellerDao();
 		
 		System.out.println(sell);
 
