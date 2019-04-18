3a97925866cf639ee85126d53408a86222ee8a48
Gilvan <camisetaecia@live.com>
2019-04-18 20:55:16 -0300

    refatorando instantiate

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,7 +12,7 @@ public class Program {
 		//Seller sell = new Seller(10, "João", "joao@teste.com", new Date(), 3000.10, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		System.out.println(" ==== TESTE Nº 01 findById ====");
 		Seller seller = sellerDao.findById(5);
 		
 		System.out.println(seller);
